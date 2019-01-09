8b2b16cd47a625dc8818041d25c082822def8acf
unknown <ivohayday@gmail.com>
2019-01-09 20:57:14 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,7 +19,8 @@ namespace DatingApp.API.Controllers
         {
             this._context = context;
         }
-        // GET api/values
+       
+       [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
